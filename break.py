6a4e4a5096ed1f5f09ed5657c7d0f9827134d761
narendras2008 <narendras2008@gmail.com>
2024-07-15 20:13:11 +0530

    python breakstatement

---
+++ break.py
@@ -0,0 +1,8 @@
+for number in [10, 20, 32, 44, 55]:
+    print(number)
+    if (number == 44):
+        
+        print("the number %d has been found" % number)
+        print("Terminate the loop")
+        
+        break
